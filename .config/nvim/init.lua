be0580f1f778f499526dc2c6f5c954570d2f31f5
Christian Doyle <christian.l.doyle@gmail.com>
2026-06-08 15:23:31 -0400

    Add ruby lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,10 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
+vim.lsp.enable('ruby_lsp')
+vim.keymap.set('n', '<leader>gd', ':lua vim.lsp.buf.definition()<CR>', { desc = 'Go to definition' })
+vim.keymap.set('n', '<leader>lr', ':lua vim.lsp.buf.references()<CR>', { desc = 'List references' })
+
 vim.api.nvim_set_option("clipboard", "unnamed")
 
 vim.keymap.set('n', '<leader>cp', ':let @*=expand("%")<CR>', { desc = 'Copy file path' })
